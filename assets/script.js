0dc8be813d7c98ce953ec7e0e020e4c4d9954bce
Benjiman Sutton <benjimansutton@gmail.com>
2021-09-30 14:35:41 +0000

    Functions added to the quiz, issue with a forEach to look at, develop more indepth workings of the quiz. EOD Commit

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,75 @@
-const quizBox = document.getElementById('quiz-container')
-const resultsShow = document.getElementById('results-box')
-const submitButton = document.getElementById('submit-btn')
 
-function createQuiz(){}
+/** this varible set the start-btn as the startButton */
+const startButton = document.getElementById('start-btn')
 
-function resultsDisplay(){}
+/** this variable lets us select the question container  */
+const questionContainerElement = document.getElementById('question-container')
+
+/** this variable gets the question element for us to put the question inside the inner text of that element */
+const questionElement = document.getElementById('question')
+
+/** this variable gets the answer button element for us to put the answer inside the inner text of each button */
+const answerButtonElement = document.getElementById('answer-buttons')
+
+/** this variable makes it show the questions shuffle and not always show up in the correct order  */
+let shuffledQuestions, currentQuestionIndex
+
+
+startButton.addEventListener('click', startGame)
+    
+
+
+/**
+ * This function will start the game, when the start button is pressed.
+ */
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+
+}
+
+
+/**
+ * CHECK THISThis will set the next question when the next button is selected, this will also change the text
+ * inside the answer buttons innertext element to show the relevant quesitons answers and disaply if they 
+ * are correct or not.
+ */
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+
+
+
+/**
+ * This function will run to get the innerText of the 
+ * question element and replace it with the question from the array of questions
+ */
+function showQuestion(question) {
+    questionElement.innerText = questions.question
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonElement.appendChild(button)
+    })
+}
+
+
+/**
+ * This will let the user select the correct answer
+ */
+function selectAnswer() {
+
+}
 
 
 /**
@@ -14,10 +79,10 @@ const questions = [
     {
         question: 'What is the capital of Chile?',
         answers: [
-            {text: 'Santiago', correct: true},
-            {text: 'London', correct: false},
-            {text: 'Mexico', correct: false},
-            {text: 'Columbia', correct: false}
+            { text: 'Santiago', correct: true },
+            { text: 'London', correct: false },
+            { text: 'Mexico', correct: false },
+            { text: 'Columbia', correct: false }
         ]
     },
     {
